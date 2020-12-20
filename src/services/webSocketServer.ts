aa59bad1af174e5afa1f0febe3967e9fc2c4bd25
Vertika Jain <vertikajain96@gmail.com>
2020-12-20 23:49:47 +0530

    Remove Chats Storage from Push Server

--- src/services/webSocketServer.ts
+++ src/services/webSocketServer.ts
@@ -5,7 +5,6 @@ import { v4 as uuid } from 'uuid';
 export default class WebSockets {
     public static wss: any
     public static connections = []
-    public static chats = new Map<string, Chat>()
 
     static init(server: http.Server): void {
 
@@ -21,7 +20,7 @@ export default class WebSockets {
             let connection = request.accept(null, request.origin)
             WebSockets.connections.push(connection)
             let senderid = request.httpRequest.url.split("/")[2]
-            connection.userID = senderid            
+            connection.userID = senderid
 
             connection.on("open", () => {
                 console.log("Server socket Connection opened.")
@@ -33,64 +32,18 @@ export default class WebSockets {
 
             connection.on('message', function (message: { utf8Data: string; }) {
                 let msgData = JSON.parse(message.utf8Data)
-                WebSockets.createPrivateChat(msgData)
+                // Create a new ID for new chat
+                if (msgData.chatId === undefined) {
+                    msgData.chatId = uuid()
+                }
+                msgData.messageId = uuid()
+                // Send message to Recipient Connection and the sender as well.
+                WebSockets.connections.map(conn => {
+                    if (conn.userID == msgData.receiverid || conn.userID == msgData.senderid) {
+                        conn.send(JSON.stringify(msgData))
+                    }
+                })
             });
         })
-    }
-
-    // Method to Create Private Conversation Space
-    static createPrivateChat(msgData: any): void {
-        if (msgData.chatid !== undefined) { 
-            // Append Message to Existing Chat
-            let chat = WebSockets.chats.get(msgData.chatid)
-            let message = new Message(chat.chatId, uuid(), msgData.message, msgData.date, msgData.senderid, msgData.receiverid)
-            chat.messages.push(message)
-            WebSockets.sendMsgtoClient(message)
-        } else { 
-            // Create New Chat
-            let chat = new Chat(uuid(), [msgData.senderid, msgData.receiverid])
-            let message = new Message(chat.chatId, uuid(), msgData.message, msgData.date, msgData.senderid, msgData.receiverid)
-            chat.messages.push(message)
-            WebSockets.chats.set(chat.chatId, chat)
-            WebSockets.sendMsgtoClient(message)
-        }
-    }
-
-    // Method to send Data to Clients
-    static sendMsgtoClient(message: Message) {
-        WebSockets.connections.map(conn => {
-            if (conn.userID == message.to || conn.userID == message.from) {
-                conn.send(JSON.stringify(message))
-            }
-        })
-    }
-}
-
-// Class for all Chats
-class Chat {
-    chatId: string
-    members: string[]
-    messages: Message[] = []
-    constructor(chatId: string, members: string[]) {
-        this.chatId = chatId
-        this.members = [...members]
-    }
-}
-
-// Class for All Messages
-class Message {
-    messageId: string
-    chatId: string
-    msg: string
-    time: Date
-    from: string
-    to: string
-    constructor(chatId: string, messageId: string, msg: string, time: Date, from: string, to: string) {
-        this.chatId = chatId
-        this.messageId = messageId
-        this.from = from
-        this.to = to
-        this.time = time
-        this.msg = msg
     }
 }
